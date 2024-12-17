1b5600be99ce0dbeddfad63e6697d9263553f53a
Daedheldir <marcin.bernacki316@gmail.com>
2024-12-17 15:40:55 +0100

    Working PN

--- active_sonar_torpedo_controller/active_sonar_torpedo_controller.lua
+++ active_sonar_torpedo_controller/active_sonar_torpedo_controller.lua
@@ -7,12 +7,14 @@
 -- Press F7 to build the project, copy the output from /_build/out/ into the game to use
 -- Remember to set your Author name etc. in the settings: CTRL+COMMA
 
-ticks = 0
+ticks = 1
 
 --[====[ EDITABLE SIMULATOR CONFIG - *automatically removed from the F7 build output ]====]
 ---@section __LB_SIMULATOR_ONLY__
 torpedo_yaw = 0
 torpedo_previous_yaw = 0
+
+test_yaws = {-0.01,0.01}
 do
     ---@type Simulator -- Set properties and screen sizes here - will run once when the script is loaded
     simulator = simulator
@@ -29,7 +31,7 @@ do
         --t1_yaw = math.fmod(0 + ticks/1000, 0.2) -0.2
         t1_yaw = 0.1
         --simulator:setInputNumber(1, t1_yaw - torpedo_yaw)
-        simulator:setInputNumber(1, -0.25 + math.fmod((torpedo_yaw/360) + rand:lbrandom_next() + (ticks / 360) / 20, 1))
+        simulator:setInputNumber(1, test_yaws[1+math.fmod(ticks, #test_yaws)])
         --simulator:setInputNumber(1, 0.1)--math.fmod(torpedo_yaw + 0.3 * math.cos(ticks / 10), 0.5))
         simulator:setInputNumber(2, 0.01)
         simulator:setInputNumber(3, torpedo_yaw + 0.3 * math.sin(ticks / 10))
@@ -50,6 +52,7 @@ do
         simulator:setProperty("APN Max Azimuth", 45)
         simulator:setProperty("Detection Azimuth Limit", 360)
         simulator:setProperty("Target Position Prediction Gain", 1)
+        simulator:setProperty("Data Averaging Period", 1)
     end;
 end
 ---@endsection
@@ -63,36 +66,10 @@ require("MathAdditions")
 -- try require("Folder.Filename") to include code from another file in this, so you can store code in libraries
 -- the "LifeBoatAPI" is included by default in /_build/libs/ - you can use require("LifeBoatAPI") to get this, and use all the LifeBoatAPI.<functions>!
 
-previous_target = {
-    number = -1,
-    yaw = 0,
-    pitch = 0,
-    yaw_relative_velocity = 0,
-    yaw_actual_velocity = 0
-}
-predicted_future_target_yaw = 0
-ANGULAR_YAW_VELOCITY = 0
-avg_yaw_velocity = LifeBoatAPI.LBRollingAverage:new(60)
-avg_angular_velocity = LifeBoatAPI.LBRollingAverage:new(30)
-previous_target_yaw_average = LifeBoatAPI.LBRollingAverage:new(30)
 
-
---Over Time Average
--- By Multiversal USB on Steam/Unimposed on Youtube
-function onTick()
-    ticks = ticks + 1
-
-    AZIMUTH_HOLD_GAIN = property.getNumber("APN Gain")
-    TARGET_VELOCITY_MULTIPLIER = property.getNumber("Target Velocity Multiplier")
-    AZIMUTH_HOLD_MIN = property.getNumber("APN Min Azimuth")
-    AZIMUTH_HOLD_MAX = property.getNumber("APN Max Azimuth")
-    TARGET_DETECTION_AZIMUTH_LIMIT = property.getNumber("Detection Azimuth Limit")
-    TARGET_POSITION_PREDICTION_GAIN = property.getNumber("Target Position Prediction Gain")
-    ANGULAR_YAW_VELOCITY = math.deg(input.getNumber(11))
-    avg_angular_velocity:lbrollingaverage_addValue(ANGULAR_YAW_VELOCITY)
-
+function getTargetsInYawRange(max_yaw, target_count)
     local found_targets = {}
-    for i = 1, 4, 1 do
+    for i = 1, target_count, 1 do
         --if target not found
         if not input.getBool(i) then
             goto continue
@@ -100,7 +77,7 @@ function onTick()
         --add all found targets to an array
         local target_number = i + i - 1
         local t_yaw = input.getNumber(target_number) * 360
-        if t_yaw < TARGET_DETECTION_AZIMUTH_LIMIT and t_yaw > -TARGET_DETECTION_AZIMUTH_LIMIT then
+        if t_yaw < max_yaw and t_yaw > -max_yaw then
             table.insert(found_targets, {
                 number = target_number,
                 yaw = t_yaw,
@@ -110,56 +87,91 @@ function onTick()
 
         ::continue::
     end
-    if #found_targets <= 0 then
-        return
-    end
+    return found_targets
+end
+
+function sortTargetsByYawDifferences(targets)
     -- loop through found targets and select the one that is closest to the previously selected target
     local yaw_differences_arr = {}
-    for key, target in pairs(found_targets) do
+    for key, target in pairs(targets) do
         table.insert(yaw_differences_arr, { predicted_future_target_yaw - target.yaw, target })
     end
 
     table.sort(yaw_differences_arr, function(left, right)
         return math.abs(left[1]) < math.abs(right[1])
     end)
-    local current_target = yaw_differences_arr[1][2]
-    previous_target_yaw_average:lbrollingaverage_addValue(current_target.yaw)
+    return yaw_differences_arr
+end
 
-    current_target.yaw_relative_velocity = (current_target.yaw - previous_target_yaw_average.average) / LifeBoatAPI.LBMaths.lbmaths_ticksToSeconds -- conversion to degrees per second
-    current_target.yaw_actual_velocity = current_target.yaw_relative_velocity + avg_angular_velocity.average
-    predicted_future_target_yaw = current_target.yaw + (current_target.yaw_relative_velocity / LifeBoatAPI.LBMaths.lbmaths_secondsToTicks)
+function calculateTargetRelativeAngularVelocityPerSecond(current_target_yaw, previous_target_yaw)
+    return (current_target_yaw - previous_target_yaw) / LifeBoatAPI.LBMaths.lbmaths_ticksToSeconds -- conversion to degrees per second
+end
 
-    avg_yaw_velocity:lbrollingaverage_addValue(current_target.yaw_actual_velocity)
+function calculateTargetAbsoluteAngularVelocityPerSecond(relative_angular_velocity, vessel_angular_velocity)
+    return relative_angular_velocity + vessel_angular_velocity -- in degrees per second
+end
 
-    lerp_yaw = math.clamp(0, 1, (math.abs(avg_yaw_velocity.average/5) * TARGET_VELOCITY_MULTIPLIER) * AZIMUTH_HOLD_GAIN)
+local DATA_AVERAGING_PERIOD = property.getNumber("Data Averaging Period")
 
-    local azimuth_offset = 0
-    local azimuth_lerp = LifeBoatAPI.LBMaths.lbmaths_lerp(AZIMUTH_HOLD_MIN, AZIMUTH_HOLD_MAX, lerp_yaw)
-    if avg_yaw_velocity.average * TARGET_VELOCITY_MULTIPLIER > 1 then
-        -- if target is moving right then put it on the left of the torpedo
-        azimuth_offset = azimuth_lerp
-    elseif avg_yaw_velocity.average * TARGET_VELOCITY_MULTIPLIER < -1 then
-        -- if target is moving left then put it on the right of the torpedo
-        azimuth_offset = -azimuth_lerp
+previous_target = {
+    number = -1,
+    yaw = 0,
+    pitch = 0,
+    yaw_relative_rate = 0,
+    yaw_actual_rate = 0
+}
+predicted_future_target_yaw = 0
+
+yaw_relative_average = LifeBoatAPI.LBRollingAverage:new(DATA_AVERAGING_PERIOD)
+yaw_actual_average = LifeBoatAPI.LBRollingAverage:new(DATA_AVERAGING_PERIOD)
+angular_velocity_average = LifeBoatAPI.LBRollingAverage:new(DATA_AVERAGING_PERIOD)
+current_target_yaw_average = LifeBoatAPI.LBRollingAverage:new(DATA_AVERAGING_PERIOD)
+desired_azimuth_change_average = LifeBoatAPI.LBRollingAverage:new(DATA_AVERAGING_PERIOD)
+--yi+1 = N(lambdai+1 - lambdai) + yi
+
+function onTick()
+    ticks = ticks + 1
+    local APN_GAIN = property.getNumber("PN Gain")
+    local TARGET_DETECTION_AZIMUTH_LIMIT = property.getNumber("Detection Azimuth Limit")
+    angular_velocity_average:lbrollingaverage_addValue(input.getNumber(31)*360)-- torpedo turn rate in degrees / second
+
+    local found_targets = getTargetsInYawRange(TARGET_DETECTION_AZIMUTH_LIMIT, 14)
+
+    if #found_targets <= 0 then
+        previous_target = {
+            number = -1,
+            yaw = 1,
+            pitch = 0,
+            yaw_relative_rate = 0,
+            yaw_actual_rate = 0,
+        }
+        return
     end
+
+    local yaw_differences_arr = sortTargetsByYawDifferences(found_targets)
+    local current_target = yaw_differences_arr[1][2]
+
+    current_target_yaw_average:lbrollingaverage_addValue(current_target.yaw)
+    current_target.yaw = current_target_yaw_average.average
+    yaw_relative_average:lbrollingaverage_addValue(calculateTargetRelativeAngularVelocityPerSecond(current_target.yaw, previous_target.yaw))
+    current_target.yaw_relative_rate = yaw_relative_average.average
+    
+    yaw_actual_average:lbrollingaverage_addValue(calculateTargetAbsoluteAngularVelocityPerSecond(current_target.yaw_relative_rate, angular_velocity_average.average))
+    current_target.yaw_actual_rate = yaw_actual_average.average
+    predicted_future_target_yaw = current_target.yaw + current_target.yaw_relative_rate
+
     --desired azimuth change in degrees per second
-    local desired_azimuth_change = azimuth_offset + (predicted_future_target_yaw * TARGET_POSITION_PREDICTION_GAIN)
-
-    --torpedo_yaw = torpedo_yaw + ANGULAR_YAW_VELOCITY / LifeBoatAPI.LBMaths.lbmaths_secondsToTicks
-    --torpedo_yaw_deg = math.deg(torpedo_yaw*LifeBoatAPI.LBMaths.lbmaths_2pi)
-    --target_deg = math.deg(current_target.yaw*LifeBoatAPI.LBMaths.lbmaths_2pi)
-    --torpedo_yaw = math.clamp(-170, 170, torpedo_yaw)
-    --ANGULAR_YAW_VELOCITY = torpedo_previous_yaw - torpedo_yaw
-    --torpedo_previous_yaw = torpedo_yaw
-
-    output.setNumber(1, desired_azimuth_change / 180)
+    desired_azimuth_change_average:lbrollingaverage_addValue(APN_GAIN * current_target.yaw_actual_rate)
+    output.setNumber(1, math.clamp(-1,1,desired_azimuth_change_average.average / LifeBoatAPI.LBMaths.lbmaths_secondsToTicks))
     output.setNumber(2, current_target.pitch)
-    output.setNumber(3, current_target.number)
-    output.setNumber(4, azimuth_offset)
-    output.setNumber(5, avg_yaw_velocity.average)
-    output.setNumber(6, current_target.yaw)
-    output.setNumber(7, current_target.yaw_actual_velocity)
-    output.setNumber(8, current_target.yaw_relative_velocity)
-
+    output.setNumber(3, current_target.yaw)
+    output.setNumber(4, current_target.number)
+    output.setNumber(5, angular_velocity_average.average)
+    output.setNumber(6, current_target.yaw_actual_rate)
+    output.setNumber(7, current_target.yaw_relative_rate)
     previous_target = current_target
 end
+
+
+-- ISSUE: When torpedo starts to move at an angle to a target then due to relative motion it appears that the target starts moving backward, even though it is still moving forward
+-- HOw to determine whether the target has started moving backward, or is just simply falling behind due to torpedo gaining on it?
